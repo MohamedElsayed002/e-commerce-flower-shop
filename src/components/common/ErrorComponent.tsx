8c439c13d2cd3aa88a73a77159e7af6b468e3977
hassanesssam <hassanessam909@gmail.com>
2025-01-22 20:45:13 +0200

    refactor(updates on initiation of the project)

--- src/components/common/ErrorComponent.tsx
+++ src/components/common/ErrorComponent.tsx
@@ -12,7 +12,7 @@ export default function ErrorComponent({ children }: ErrorComponentProps) {
     <section className="flex flex-col gap-6 text-center items-center">
       <span className="text-9xl text-red-500 font-bold">{t("error")}</span>
 
-      {/* Error Message*/}
+      {/* Error Message */}
       <p className="px-3 py-2 rounded-full bg-red-50 text-red-400 border border-red-500">
         {children || t("fallback-error-message")}
       </p>
