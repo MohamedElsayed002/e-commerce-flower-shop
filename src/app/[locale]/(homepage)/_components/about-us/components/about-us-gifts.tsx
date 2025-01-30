eee36c0b78a638b240b4c1c9e5c9dd21baab2c9e
MariemMohamed04 <mariemmohamed1421@gmail.com>
2025-01-30 11:59:12 +0200

    refactor(home-screen-2): "Adding sizes attribute to images."

--- src/app/[locale]/(homepage)/_components/about-us/components/about-us-gifts.tsx
+++ src/app/[locale]/(homepage)/_components/about-us/components/about-us-gifts.tsx
@@ -7,9 +7,15 @@ export default function AboutUsGifts() {
       {/* Main gift box */}
       <div
         className="about-us-gift-one z-10 relative w-[302px] h-[344px] mt-[24.21px] rounded-b-[120px] overflow-hidden
-          ml-[27.49px] rounded-tl-[50px] rounded-tr-[120px] rtl:mr-[27.49px] rtl:rounded-tr-[50px] rtl:rounded-tl-[120px]"
+      ml-[27.49px] rounded-tl-[50px] rounded-tr-[120px] rtl:mr-[27.49px] rtl:rounded-tr-[50px] rtl:rounded-tl-[120px]"
       >
-        <Image src="/assets/images/gift-box-1.png" alt="gift 1" fill className="object-cover" />
+        <Image
+          src="/assets/images/gift-box-1.png"
+          alt="gift 1"
+          fill
+          sizes="(max-width: 640px) 150px, (max-width: 1024px) 250px, 302px"
+          className="object-cover"
+        />
       </div>
 
       {/* Border decoration */}
@@ -23,12 +29,24 @@ export default function AboutUsGifts() {
       <div className="flex flex-col pt-[15.97px] gap-2">
         {/* Second gift box */}
         <div className="about-us-gift-two relative w-[193px] h-[193px] rounded-full">
-          <Image src="/assets/images/gift-box-2.png" alt="gift 2" fill className="rounded-full" />
+          <Image
+            src="/assets/images/gift-box-2.png"
+            alt="gift 2"
+            fill
+            sizes="(max-width: 640px) 100px, (max-width: 1024px) 150px, 193px"
+            className="rounded-full"
+          />
         </div>
 
         {/* Third gift box */}
         <div className="about-us-gift-three relative w-[193px] h-[144px] rounded-s-[50px] rounded-e-[100px] overflow-hidden">
-          <Image src="/assets/images/gift-box-3.png" alt="gift 3" fill className="object-cover" />
+          <Image
+            src="/assets/images/gift-box-3.png"
+            alt="gift 3"
+            fill
+            sizes="(max-width: 640px) 100px, (max-width: 1024px) 150px, 193px"
+            className="object-cover"
+          />
         </div>
       </div>
     </div>
--- src/components/custom/product-card-component.tsx
+++ src/components/custom/product-card-component.tsx
@@ -14,81 +14,80 @@ export default function ProductCard({ product }: ProductCardProps) {
   const format = useFormatter();
 
   return (
-      <Card className="w-[302px] rounded-[20px]" key={product.id}>
-        {/* Card header */}
-        <CardHeader className="group bg-main-color rounded-[20px] flex justify-center items-center mb-4 overflow-hidden relative">
-          {/* Image overlay */}
-          <div className="absolute inset-0 bg-custom-rose-900/70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-[20px]"></div>
+    <Card className="w-[302px] rounded-[20px]" key={product.id}>
+      {/* Card header */}
+      <CardHeader className="group bg-main-color rounded-[20px] flex justify-center items-center mb-4 overflow-hidden relative">
+        {/* Image overlay */}
+        <div className="absolute inset-0 bg-custom-rose-900/70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-[20px]"></div>
+        {/* Product image */}
+        <div className="relative w-[222px] h-[222px] mt-5 mb-[30px]">
+          <Image
+            src={product.imgCover}
+            alt={product.title}
+            fill
+            sizes="(max-width: 640px) 100px, (max-width: 1024px) 150px, 222px"
+            className="object-cover group-hover:opacity-70 transition-opacity duration-300 z-10"
+          />
+        </div>
+        
+        {/* Action buttons */}
+        <div className="absolute inset-0 flex justify-center items-center gap-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
+          {/* TODO: View Product Button */}
+          <Button className="bg-custom-rose-900 text-2xl w-10 h-10 rounded-full flex justify-center items-center text-white hover:bg-custom-rose-800">
+            <FaRegEye />
+          </Button>
 
-          {/* Product image */}
-          <div className="relative w-[222px] h-[222px] mt-5 mb-[30px]">
-            <Image
-              src={product.imgCover}
-              alt={product.title}
-              fill
-              className="object-cover group-hover:opacity-70 transition-opacity duration-300 z-10"
-            />
-          </div>
+          {/* TODO:Add to wishlist button */}
+          <Button className="bg-custom-rose-900 text-2xl w-10 h-10 rounded-full flex justify-center items-center text-white hover:bg-custom-rose-800">
+            <FaRegHeart />
+          </Button>
+        </div>
+      </CardHeader>
 
-          {/* Action buttons */}
-          <div className="absolute inset-0 flex justify-center items-center gap-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
-            {/* TODO: View Product Button */}
-            <Button className="bg-custom-rose-900 text-2xl w-10 h-10 rounded-full flex justify-center items-center text-white hover:bg-custom-rose-800">
-              <FaRegEye />
-            </Button>
-            
-            {/* TODO:Add to wishlist button */}
-            <Button className="bg-custom-rose-900 text-2xl w-10 h-10 rounded-full flex justify-center items-center text-white hover:bg-custom-rose-800">
-              <FaRegHeart />
-            </Button>
-          </div>
-        </CardHeader>
+      {/* Product details */}
+      <CardContent className="px-4">
+        <div className="product-data flex justify-between items-center">
+          {/* Product information */}
+          <div className="flex flex-col justify-start gap-[9px]">
+            {/* Product title */}
+            <h6 className="text-start text-[17px] font-semibold text-blue-gray-900 font-inter line-clamp-1">
+              {product.title}
+            </h6>
 
-        {/* Product details */}
-        <CardContent className="px-4">
-          <div className="product-data flex justify-between items-center">
-            {/* Product information */}
-            <div className="flex flex-col justify-start gap-[9px]">
-              {/* Product title */}
-              <h6 className="text-start text-[17px] font-semibold text-blue-gray-900 font-inter line-clamp-1">
-                {product.title}
-              </h6>
-
-              {/* TODO: Product rating */}
-              <div className="flex text-rate-color">
-                {[...Array(5)].map((_, index) => (
-                  <span key={index}>
-                    {index < (product?.rating || 4) ? <FaStar /> : <FaRegStar />}
-                  </span>
-                ))}
-              </div>
-
-              {/* Product price */}
-              <p className="text-base text-flamingo font-medium text-start font-roboto">
-                {/* Formatting price */}
-                {format.number(product.priceAfterDiscount || product.price, {
-                  style: "currency",
-                  currency: "USD",
-                })}{" "}
-
-                {/* Display discounted price if available */}
-                {product.priceAfterDiscount && (
-                  <span className="line-through text-blue-gray-50">
-                    {format.number(product.price, {
-                      style: "currency",
-                      currency: "USD",
-                    })}
-                  </span>
-                )}
-              </p>
+            {/* TODO: Product rating */}
+            <div className="flex text-rate-color">
+              {[...Array(5)].map((_, index) => (
+                <span key={index}>
+                  {index < (product?.rating || 4) ? <FaStar /> : <FaRegStar />}
+                </span>
+              ))}
             </div>
 
-            {/* TODO:Add to cart button */}
-            <Button className="text-white bg-custom-purple-900 w-[42px] h-[42px] rounded-full flex justify-center items-center hover:bg-custom-purple-800">
-              <BsHandbag />
-            </Button>
+            {/* Product price */}
+            <p className="text-base text-flamingo font-medium text-start font-roboto">
+              {/* Formatting price */}
+              {format.number(product.priceAfterDiscount || product.price, {
+                style: "currency",
+                currency: "USD",
+              })}{" "}
+              {/* Display discounted price if available */}
+              {product.priceAfterDiscount && (
+                <span className="line-through text-blue-gray-50">
+                  {format.number(product.price, {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </span>
+              )}
+            </p>
           </div>
-        </CardContent>
-      </Card>
+
+          {/* TODO:Add to cart button */}
+          <Button className="text-white bg-custom-purple-900 w-[42px] h-[42px] rounded-full flex justify-center items-center hover:bg-custom-purple-800">
+            <BsHandbag />
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
