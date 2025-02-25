fea24cff60b86350f21887c773f6c73d48c28910
MariemMohamed04 <mariemmohamed1421@gmail.com>
2025-02-25 22:59:37 +0200

    feat(set-password-form): "Formatting."

--- src/app/[locale]/(client)/(homepage)/page.tsx
+++ src/app/[locale]/(client)/(homepage)/page.tsx
@@ -7,8 +7,6 @@ import PopularItems from "./_components/popular-items";
 import GallerySection from "./_components/gallery";
 import TestimonialSection from "./_components/testimonials";
 import CompaniesSection from "./_components/partner-companies";
-import { Toaster } from "@/components/ui/sonner";
-import SetPasswordForm from "@/components/features/auth/components/set-password-form";
 
 export default async function Home({ searchParams }: { searchParams: SearchParams }) {
   // Variables
@@ -17,28 +15,25 @@ export default async function Home({ searchParams }: { searchParams: SearchParam
   return (
     <main className="w-full">
       {/* Categories */}
-      {/* <Categories categories={payload?.categories || []} /> */}
+      <Categories categories={payload?.categories || []} />
 
       {/* Best Seller */}
-      {/* <BestSeller /> */}
+      <BestSeller />
 
       {/* Popular Items */}
-      {/* <PopularItems searchParams={searchParams} categories={payload?.categories || []} /> */}
+      <PopularItems searchParams={searchParams} categories={payload?.categories || []} />
 
       {/* About Us */}
-      {/* <AboutUs /> */}
+      <AboutUs />
 
       {/* Gallery */}
-      {/* <GallerySection /> */}
+      <GallerySection />
 
       {/* Testimonials */}
-      {/* <TestimonialSection /> */}
+      <TestimonialSection />
 
       {/* Companies */}
-      {/* <CompaniesSection /> */}
-
-      <Toaster position="top-center" />
-      <SetPasswordForm />
+      <CompaniesSection />
     </main>
   );
 }
--- src/components/features/auth/components/set-password-form.tsx
+++ src/components/features/auth/components/set-password-form.tsx
@@ -13,7 +13,7 @@ import dynamic from "next/dynamic";
 import { useState } from "react";
 
 // Lazy Loading
-const LoginForm = dynamic(() => import('./dummy-login-form'), {
+const LoginForm = dynamic(() => import("./dummy-login-form"), {
   ssr: false,
   loading: () => <p>Loading...</p>,
 });
@@ -22,7 +22,7 @@ export default function SetPasswordForm() {
   // Translation
   const t = useTranslations();
 
-// State
+  // State
   const [showLoginForm, setShowLoginForm] = useState(false);
 
   // Mutation
@@ -56,15 +56,18 @@ export default function SetPasswordForm() {
 
   // Functions
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    mutate({ email: data.email, newPassword: data.newPassword }, {
-      onSuccess: () => {
-        toast.success("Password updated successfully");
-        setShowLoginForm(true);
+    mutate(
+      { email: data.email, newPassword: data.newPassword },
+      {
+        onSuccess: () => {
+          toast.success("Password updated successfully");
+          setShowLoginForm(true);
+        },
+        onError: (error) => {
+          toast.error(error.message);
+        },
       },
-      onError: (error) => {
-        toast.error(error.message);
-      },
-    });
+    );
   };
 
   return (
@@ -84,7 +87,7 @@ export default function SetPasswordForm() {
                     <Input
                       type="email"
                       {...field}
-                      placeholder={t('enter-e-mail')}
+                      placeholder={t("enter-e-mail")}
                       className="w-[528px] h-[52px] rounded-[20px] p-2"
                       style={{ boxShadow: "0px 1px 10px 0px rgba(0, 0, 0, 0.1)" }}
                     />
@@ -143,7 +146,7 @@ export default function SetPasswordForm() {
               disabled={isPending}
               className="bg-custom-rose-900 w-[528px] rounded-[30px] px-[31px] font-medium text-base hover:bg-custom-rose-800"
             >
-              {isPending ? t('setting-new-password') : t('set-password')}
+              {isPending ? t("setting-new-password") : t("set-password")}
             </Button>
           </form>
         </Form>
