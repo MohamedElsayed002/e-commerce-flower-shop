3caa16986e4d4f615495bfc7862f17c3f4b80692
sofamo22 <seif011523@gmail.com>
2025-02-27 21:43:24 +0200

    fixed unused comments, changed the dropdown ui, removed unused variables

--- src/components/common/profile-icon.tsx
+++ src/components/common/profile-icon.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useTranslations } from 'next-intl'
 import React from 'react'
 import {
@@ -6,17 +7,14 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
-import { getSession, signOut } from 'next-auth/react'
-import { Link, redirect } from '@/i18n/routing'
-import Image from 'next/image'  
+import { signOut } from 'next-auth/react'
+import { Link } from '@/i18n/routing'
 import { Button } from '../ui/button'
 import { CgProfile } from "react-icons/cg";
 
 export default function ProfileIcon() {
-    // translations
+    // Translations
     const t = useTranslations()
-    //check if user is signed in
-    const userSession = getSession()
     
     return (
         <DropdownMenu>
@@ -24,12 +22,12 @@ export default function ProfileIcon() {
           <DropdownMenuTrigger><CgProfile className='text-custom-rose-800 w-5 h-5'/></DropdownMenuTrigger>
           {/* Links */}
           <DropdownMenuContent>
-              {/* Profile page */}
-            <DropdownMenuItem><Link href="/profile">{t("profile")}</Link></DropdownMenuItem>
+            {/* Profile page */}
+            <Link href="/profile"><DropdownMenuItem className='focus:bg-custom-rose-600 hover:cursor-pointer focus:text-white p-2'>{t("profile")}</DropdownMenuItem></Link>
             {/* Orders Page */}
-            <DropdownMenuItem><Link href="/orders">{t("orders-page")}</Link></DropdownMenuItem>
+            <Link  href="/orders"><DropdownMenuItem className='focus:bg-custom-rose-600 hover:cursor-pointer focus:text-white p-2'>{t("orders-page")}</DropdownMenuItem></Link>
             {/* Log Out */}
-            <DropdownMenuItem><Button onClick={() => {signOut()}}>{t("signout")}</Button></DropdownMenuItem>
+            <DropdownMenuItem><Button onClick={() => signOut()} className='text-white bg-custom-rose-800 hover:bg-custom-rose-500 '>{t("signout")}</Button></DropdownMenuItem>
           </DropdownMenuContent>
           </DropdownMenu>
     )
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+'use client'
 import React from "react";
 import Image from "next/image";
 import { FaRegHeart } from "react-icons/fa6";
@@ -9,14 +8,16 @@ import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
 import LocaleToggle from "@/components/common/toggle-locale";
 import ProfileIcon from "../common/profile-icon";
-import { useSession } from "next-auth/react";
 import { Button } from "../ui/button";
+import { } from "next-auth";
+import { useSession } from "next-auth/react";
 
 export default function Header() {
   // Translation
   const t = useTranslations();
+  
   // Session
-  const { data: session } = useSession()
+  const { data: session}  = useSession()
 
   return (
     <header>
@@ -52,7 +53,8 @@ export default function Header() {
           <FaRegHeart className="w-5 h-5 text-custom-rose-900" />
           <IoLockClosedOutline className="w-[30px] h-5 text-custom-rose-900" />
           <LocaleToggle />
-          {session ? <ProfileIcon/> : <Button>{t('Login')}</Button>}
+          {/* {session ? <ProfileIcon/> : <Button>{t('Login')}</Button>} */}
+          <ProfileIcon/>
         </div>
       </div>
     </header>
