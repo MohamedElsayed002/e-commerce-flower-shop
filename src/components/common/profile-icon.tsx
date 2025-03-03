07c221bae9f31fd4a3e8fe2e073a1c1d0ba44ff2
sofamo22 <seif011523@gmail.com>
2025-03-03 06:40:55 +0200

    new branch for the user button

---
+++ src/components/common/profile-icon.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { useTranslations } from 'next-intl'
+import React from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import { signOut } from 'next-auth/react'
+import { Link } from '@/i18n/routing'
+import { Button } from '../ui/button'
+import { CgProfile } from "react-icons/cg";
+
+export default function ProfileIcon() {
+    // Translations
+    const t = useTranslations()
+    
+    return (
+        <DropdownMenu>
+          {/*  Profile Icon */}
+          <DropdownMenuTrigger><CgProfile className='text-custom-rose-800 w-5 h-5'/></DropdownMenuTrigger>
+          {/* Links */}
+          <DropdownMenuContent>
+            {/* Profile page */}
+            <Link href="/profile"><DropdownMenuItem className='focus:bg-custom-rose-600 hover:cursor-pointer focus:text-white p-2'>{t("profile")}</DropdownMenuItem></Link>
+            {/* Orders Page */}
+            <Link  href="/orders"><DropdownMenuItem className='focus:bg-custom-rose-600 hover:cursor-pointer focus:text-white p-2'>{t("orders-page")}</DropdownMenuItem></Link>
+            {/* Log Out */}
+            <DropdownMenuItem><Button onClick={() => signOut()} className='text-white bg-custom-rose-800 hover:bg-custom-rose-500 '>{t("signout")}</Button></DropdownMenuItem>
+          </DropdownMenuContent>
+          </DropdownMenu>
+    )
+}
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+'use client'
 import React from "react";
 import Image from "next/image";
 import { FaRegHeart } from "react-icons/fa6";
@@ -8,10 +7,17 @@ import { IoSearch } from "react-icons/io5";
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
 import LocaleToggle from "@/components/common/toggle-locale";
+import ProfileIcon from "../common/profile-icon";
+import { Button } from "../ui/button";
+import { } from "next-auth";
+import { useSession } from "next-auth/react";
 
 export default function Header() {
   // Translation
   const t = useTranslations();
+  
+  // Session
+  const { data: session}  = useSession()
 
   return (
     <header>
@@ -47,6 +53,7 @@ export default function Header() {
           <FaRegHeart className="w-5 h-5 text-custom-rose-900" />
           <IoLockClosedOutline className="w-[30px] h-5 text-custom-rose-900" />
           <LocaleToggle />
+           {session ? <ProfileIcon/> : <Button>{t('Login')}</Button>}          
         </div>
       </div>
     </header>
