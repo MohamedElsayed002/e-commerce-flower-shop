5fa94991f3f09c4c6b7cd3cf564db224e4b7ff7e
Mohamed Elsayed <mohamed.madoo2016@gmail.com>
2025-04-05 00:53:39 +0200

    feat/create-status-filter

---
+++ src/app/[locale]/(client)/(homepage)/all-categories/page.tsx
@@ -0,0 +1,10 @@
+import { FilterStatus } from "./_components/filter-status";
+
+export default function AllCategoriesPage() {
+  return (
+    <div>
+      {/* Filter Status */}
+      <FilterStatus />
+    </div>
+  );
+}
