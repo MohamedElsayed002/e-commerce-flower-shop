b7aa1ed781c432247d7414c550a28db28bf599e3
Ahmed-hamdi22 <112027799+Ahmed-hamdi22@users.noreply.github.com>
2025-04-05 20:49:58 +0200

    solve import

--- src/app/[locale]/(client)/products/page.tsx
+++ src/app/[locale]/(client)/products/page.tsx
@@ -1,14 +1,10 @@
 import { fetchCategories } from "@/lib/apis/category.api";
 import CategoryFilter from "./_components/filters/categoty-filter";
-import { fetchOccasions } from "@/lib/apis/occasion-api";
-import OccasionFilter from "./_components/filters/occasion-filter";
-import PriceFilter from "./_components/filters/price-filter";
 
 export default async function ProductPage() {
   const categoriesData = await fetchCategories();
-  const occasionsData = await fetchOccasions();
 
-  if (!categoriesData || !occasionsData) {
+  if (!categoriesData) {
     return <p>Error loading filters.</p>;
   }
 
@@ -16,9 +12,6 @@ export default async function ProductPage() {
     <div className="p-6 space-y-6">
       {/* Filters */}
       <CategoryFilter categories={categoriesData.categories} />
-      <OccasionFilter occasions={occasionsData.occasions} />
-      <PriceFilter />
-      {/* Render product list here */}
     </div>
   );
 }
