8855b7f72b412c174687c91825d11eca7051efd1
assmaayaser17 <yaserassmaa@gmail.com>
2025-04-07 16:39:20 +0200

    done

--- src/app/[locale]/(client)/products/[productid]/_components/product-details/product-content/product-content.tsx
+++ src/app/[locale]/(client)/products/[productid]/_components/product-details/product-content/product-content.tsx
@@ -18,20 +18,20 @@ export default async function Content({ product }: Productprops) {
           {/* Product title */}
           <h2 className="text-[25px] font-semibold">{product.title}</h2>
 
-          <div className="flex gap-[5px]  items-center ">
+          <div className="flex gap-[5px] items-center ">
             {/* Product price */}
-            <span className="text-gray-600 line-through text-[18px] font-medium">
+            <span className="text-gray-600 line-through text-[18px] font-medium ">
               <h1>${product.price}</h1>
             </span>
 
             {/* Price after discount */}
-            <span className="text-custom-rose-900 text-[23px] font-medium">
+            <span className="text-custom-rose-900 text-[23px] font-medium ">
               {" "}
               <h1>${product.priceAfterDiscount}</h1>
             </span>
 
             {/* Product discount */}
-            <span className="text-red-500 text-[15px] font-medium">
+            <span className="text-red-500 text-[15px] font-medium ">
               <h1>
                 {product.discount}
                 {t("off")}
@@ -40,7 +40,7 @@ export default async function Content({ product }: Productprops) {
           </div>
 
           {/* Product description */}
-          <p className="text-[#757F95] text-[16px] font-normal leading-[28.8px]">
+          <p className="text-[#757F95] text-[16px] font-normal leading-[28.8px] mb-5">
             {product.description}
           </p>
 
