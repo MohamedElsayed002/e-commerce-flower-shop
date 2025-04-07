b891d141152fe899b47b7c5008845cc250e32539
Ahmed-hamdi22 <112027799+Ahmed-hamdi22@users.noreply.github.com>
2025-04-07 03:00:31 +0200

    remove-occ&price

--- src/app/[locale]/(client)/products/page.tsx
+++ src/app/[locale]/(client)/products/page.tsx
@@ -1,19 +1,13 @@
-import { fetchOccasions } from "@/lib/apis/occasion-api";
-import OccasionFilter from "./_components/filters/occasion-filter";
 import { fetchCategories } from "@/lib/apis/category.api";
 import CategoryFilter from "./_components/filters/categoty-filter";
-import PriceFilter from "./_components/filters/price-filter";
 
 export default async function ProductPage() {
-  const occasionsData = await fetchOccasions();
   const categoriesData = await fetchCategories();
 
   return (
     <div className="p-6 space-y-6">
       {/* Filters */}
-      <OccasionFilter occasions={occasionsData?.occasions ?? []} />
       <CategoryFilter categories={categoriesData?.categories ?? []} />
-      <PriceFilter />
     </div>
   );
 }
--- src/lib/types/occasion.d.ts
+++
@@ -1,7 +0,0 @@
-declare type Occasion = {
-  name: string;
-  image: string;
-  productsCount: number;
-  slug: string;
-  id: string;
-} & DatabaseFields;
