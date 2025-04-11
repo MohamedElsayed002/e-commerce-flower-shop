9ac0caac3d0947e7ea9f52b99afa13aaad90b093
Ahmed-hamdi22 <112027799+Ahmed-hamdi22@users.noreply.github.com>
2025-04-11 05:12:09 +0200

    update function

--- src/lib/apis/category.api.ts
+++ src/lib/apis/category.api.ts
@@ -1,38 +1,8 @@
-// "use server";
-
-// export async function fetchCategories() {
-//   try {
-//     const response = await fetch(`${process.env.API}/categories`, {
-//       cache: "no-store",
-//     });
-
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch categories");
-//     }
-
-//     const payload: APIResponse<PaginatedResponse<{ categories: Category[] }>> =
-//       await response.json();
-
-//     if ("error" in payload) {
-//       throw new Error(payload.error);
-//     }
-
-//     return payload;
-//   } catch (error) {
-//     console.error("Error fetching categories: ", error);
-//     return null;
-//   }
-// }
 "use server";
 
-export async function fetchCategories(category?: string) {
+export async function fetchCategories() {
   try {
-    // بناء رابط الطلب بناءً على قيمة الفئة (إن وجدت)
-    const url = category
-      ? `${process.env.API}/categories?category=${encodeURIComponent(category)}`
-      : `${process.env.API}/categories`;
-
-    const response = await fetch(url, {
+    const response = await fetch(`${process.env.API}/categories`, {
       cache: "no-store",
     });
 
