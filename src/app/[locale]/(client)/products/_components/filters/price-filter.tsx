8c99af03ad12eac02d07361fd04a07b9d71b650a
Ahmed-hamdi22 <112027799+Ahmed-hamdi22@users.noreply.github.com>
2025-04-12 01:03:35 +0200

    remove reactquery&maxpricefuc

--- src/app/[locale]/(client)/products/_components/filters/price-filter.tsx
+++ src/app/[locale]/(client)/products/_components/filters/price-filter.tsx
@@ -4,24 +4,22 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useDebouncedCallback } from "use-debounce";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { Slider } from "@/components/ui/slider";
 import { Card } from "@/components/ui/card";
 import { useTranslations } from "next-intl";
-import { Loader2 } from "lucide-react";
-import { useMaxPriceFromProducts } from "@/hooks/filters-hook/use-price";
+import { usePathname, useRouter } from "@/i18n/routing";
 
 export default function PriceFilter() {
   // Translations
   const t = useTranslations();
 
-  // Router
+  // Variables
   const router = useRouter();
-
-  // Params
   const searchParams = useSearchParams();
+  const pathname = usePathname();
 
-  // Validation schema
+  // Validation
   const priceFilterSchema = z.object({
     priceRange: z
       .tuple([z.number().nonnegative(), z.number().nonnegative()])
@@ -29,13 +27,10 @@ export default function PriceFilter() {
         message: t("price_range_error"),
       }),
   });
-
-  // Type for form data
   type PriceFilterFormType = z.infer<typeof priceFilterSchema>;
 
-  // Fetch max price from API
-  const { data: apiMax, isLoading, isError } = useMaxPriceFromProducts();
-  const maxPrice = apiMax && !isError ? apiMax : 10000;
+  // Max price
+  const maxPrice = 30000;
 
   // Get initial values from URL
   const initialMin = Number(searchParams.get("price[gte]")) || 0;
@@ -59,14 +54,13 @@ export default function PriceFilter() {
     params.set("price[gte]", String(min));
     params.set("price[lte]", String(max));
     // Update the URL
-    router.replace(`?${params.toString()}`);
+    router.replace(`${pathname}?${params.toString()}`);
   }, 300);
 
   // On slider change
-  const handleSliderChange = (value: number[]) => {
-    const tupleValue = value as [number, number];
-    setValue("priceRange", tupleValue, { shouldValidate: true });
-    debouncedReplace(tupleValue);
+  const handleSliderChange = (value: [number, number]) => {
+    setValue("priceRange", value, { shouldValidate: true });
+    debouncedReplace(value);
   };
 
   return (
@@ -76,31 +70,22 @@ export default function PriceFilter() {
         {t("price_rating")}
       </h3>
 
-      {/* Loading state */}
-      {isLoading ? (
-        <div className="flex justify-center items-center h-16">
-          <Loader2 className="animate-spin w-6 h-6 text-custom-rose-700" />
-        </div>
-      ) : (
-        <>
-          {/* Price range */}
-          <div className="flex text-sm text-custom-rose-900 font-bold rtl:flex-row-reverse">
-            <span className="me-1">${priceRange[0].toFixed(2)}</span>
-            {"-"}
-            <span className="ms-1">${priceRange[1].toFixed(2)}</span>
-          </div>
+      {/* Price range */}
+      <div className="flex text-sm text-custom-rose-900 font-bold rtl:flex-row-reverse">
+        <span className="me-1">${priceRange[0].toFixed(2)}</span>
+        {"-"}
+        <span className="ms-1">${priceRange[1].toFixed(2)}</span>
+      </div>
 
-          {/* Slider */}
-          <Slider
-            className="w-[254px]  h-[16px] text-custom-rose-900 rtl:direction-ltr"
-            min={0}
-            max={maxPrice}
-            step={1}
-            value={priceRange}
-            onValueChange={handleSliderChange}
-          />
-        </>
-      )}
+      {/* Slider */}
+      <Slider
+        className="w-[254px]  h-[16px] text-custom-rose-900 rtl:direction-ltr"
+        min={0}
+        max={maxPrice}
+        step={10}
+        value={priceRange}
+        onValueChange={handleSliderChange}
+      />
     </Card>
   );
 }
