986895ec2a7f80b604d77e4533dbca006017d0ae
Ahmed-hamdi22 <112027799+Ahmed-hamdi22@users.noreply.github.com>
2025-04-11 04:33:47 +0200

    handel function

--- src/hooks/filters-hook/use-category.ts
+++ src/hooks/filters-hook/use-category.ts
@@ -1,5 +1,6 @@
 "use client";
 
+import { fetchCategories } from "@/lib/apis/category.api";
 import { useQuery } from "@tanstack/react-query";
 
 // Hook to fetch products by category
@@ -7,14 +8,6 @@ export function useProductsByCategory(category?: string) {
   // Fetch products
   return useQuery({
     queryKey: ["products", category],
-    queryFn: async () => {
-      const url = new URL(process.env.API + "/products");
-      if (category) {
-        url.searchParams.append("category", category);
-      }
-      const res = await fetch(url.toString());
-      if (!res.ok) throw new Error("Failed to fetch products");
-      return res.json();
-    },
+    queryFn: () => fetchCategories(category),
   });
 }
--- src/lib/apis/category.api.ts
+++ src/lib/apis/category.api.ts
@@ -1,9 +1,40 @@
-export async function fetchCategories() {
+// "use server";
+
+// export async function fetchCategories() {
+//   try {
+//     const response = await fetch(`${process.env.API}/categories`, {
+//       cache: "no-store",
+//     });
+
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch categories");
+//     }
+
+//     const payload: APIResponse<PaginatedResponse<{ categories: Category[] }>> =
+//       await response.json();
+
+//     if ("error" in payload) {
+//       throw new Error(payload.error);
+//     }
+
+//     return payload;
+//   } catch (error) {
+//     console.error("Error fetching categories: ", error);
+//     return null;
+//   }
+// }
+"use server";
+
+export async function fetchCategories(category?: string) {
   try {
-    const response = await fetch(`${process.env.API}/products?category`, {
+    // بناء رابط الطلب بناءً على قيمة الفئة (إن وجدت)
+    const url = category
+      ? `${process.env.API}/categories?category=${encodeURIComponent(category)}`
+      : `${process.env.API}/categories`;
+
+    const response = await fetch(url, {
       cache: "no-store",
     });
-    // =${category}
 
     if (!response.ok) {
       throw new Error("Failed to fetch categories");
