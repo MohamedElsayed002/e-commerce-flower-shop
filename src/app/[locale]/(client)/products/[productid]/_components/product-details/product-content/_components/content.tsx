5a4f171344503973f08af3375bc9841a0cc80fbc
assmaayaser17 <yaserassmaa@gmail.com>
2025-04-17 23:07:53 +0200

    done

--- src/app/[locale]/(client)/products/[productid]/_components/product-details/product-content/_components/content.tsx
+++ src/app/[locale]/(client)/products/[productid]/_components/product-details/product-content/_components/content.tsx
@@ -13,61 +13,61 @@ export default async function Content({ product }: Productprops) {
 
   return (
     <div className=" flex flex-col  w-[550px] ">
-    <div className=" flex flex-col w-[550px] ">
-      <div className="flex flex-col ">
-        {/* Product title */}
-        <h2 className="text-[25px] font-semibold">{product.title}</h2>
+      <div className=" flex flex-col w-[550px] ">
+        <div className="flex flex-col ">
+          {/* Product title */}
+          <h2 className="text-[25px] font-semibold">{product.title}</h2>
 
-        {/* Details */}
-        <div className="flex gap-[5px] items-center mt-4 ">
-          {/* Product price */}
-          <p className="text-gray-600 line-through text-[18px]  font-medium ">
-            {format.number(product.price, {
-              currency: "USD",
-              style: "currency",
-            })}
+          {/* Details */}
+          <div className="flex gap-[5px] items-center mt-4 ">
+            {/* Product price */}
+            <p className="text-gray-600 line-through text-[18px]  font-medium ">
+              {format.number(product.price, {
+                currency: "USD",
+                style: "currency",
+              })}
+            </p>
+
+            {/* Price after discount */}
+            <p className="text-custom-rose-900 text-[23px] font-medium ">
+              {format.number(product.priceAfterDiscount, {
+                currency: "USD",
+                style: "currency",
+              })}
+            </p>
+
+            {/* Product discount */}
+            <p className="text-red-500 text-[15px] font-medium ">
+              {format.number(product.discount, {
+                style: "percent",
+              })}
+              {t("off")}
+            </p>
+          </div>
+
+          {/* Product description */}
+          <p className="text-[#757F95] text-[16px] font-normal leading-[28.8px] mt-5">
+            {product.description}
           </p>
 
-          {/* Price after discount */}
-          <p className="text-custom-rose-900 text-[23px] font-medium ">
-            {format.number(product.priceAfterDiscount, {
-              currency: "USD",
-              style: "currency",
-            })}
-          </p>
-
-          {/* Product discount */}
-          <p className="text-red-500 text-[15px] font-medium ">
-            {format.number(product.discount, {
-              style: "percent",
-            })}
-            {t("off")}
-          </p>
+          {/* Stock status */}
+          <li className="text-[16px] font-medium gap-2 text-blue-gray-500 mt-4">
+            {t("stock")}:{" "}
+            {product.quantity > 0 ? (
+              <span className=" text-[16px] font-normal text-blue-gray-500">{t("in-stock")}</span>
+            ) : (
+              <span className="text-red-500">{t("out-of-stock")}</span>
+            )}
+          </li>
         </div>
 
-        {/* Product description */}
-        <p className="text-[#757F95] text-[16px] font-normal leading-[28.8px] mt-5">
-          {product.description}
-        </p>
-
-        {/* Stock status */}
-        <li className="text-[16px] font-medium gap-2 text-blue-gray-500 mt-4">
-          {t("stock")}:{" "}
-          {product.quantity > 0 ? (
-            <span className=" text-[16px] font-normal text-blue-gray-500">{t("in-stock")}</span>
-          ) : (
-            <span className="text-red-500">{t("out-of-stock")}</span>
+        {/* Check quantity && Quantity selector */}
+        <div className="mt-4">
+          {product.quantity > 0 && (
+            <QuantitySelector maxQuantity={product.quantity} productid={product._id} />
           )}
-        </li>
+        </div>
       </div>
-
-      {/* Check quantity && Quantity selector */}
-      <div className="mt-4">
-        {product.quantity > 0 && (
-          <QuantitySelector maxQuantity={product.quantity} productid={product._id} />
-        )}
-      </div>
-    </div>
     </div>
   );
 }
