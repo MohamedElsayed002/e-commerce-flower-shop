f745071b959270343299891009c5c30006681995
MariemMohamed04 <mariemmohamed1421@gmail.com>
2025-04-21 17:21:08 +0200

    feat(informational-pages): "Formatting header file."

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
               <FaRegHeart className="w-5 h-5 text-custom-rose-900" />
               <IoLockClosedOutline className="w-[30px] h-5 text-custom-rose-900" />
               <LocaleToggle />
-              <ProfileIcon />          
+              <ProfileIcon />
             </>
           )}
 
