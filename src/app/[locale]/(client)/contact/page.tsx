e1741016be1407bf41b985e9f541bc8a36902eb6
assmaayaser17 <yaserassmaa@gmail.com>
2025-04-22 17:13:04 +0200

    done

--- src/app/[locale]/(client)/contact/page.tsx
+++ src/app/[locale]/(client)/contact/page.tsx
@@ -11,7 +11,9 @@ export default function page() {
   return (
     <>
       <div className="container m-auto flex flex-col my-10 ">
-        <h1 className=" text-custom-rose-900 text-[17px] font-bold mt-5">{t("contact-us")}</h1>
+        <h1 className=" text-custom-rose-900 text-[17px] font-bold mt-5 leading-[30.6px] tracking-[0.15em]">
+          {t("contact-us")}
+        </h1>
         <div className="flex justify-between items-center">
           {/* Contact Icons */}
           <ContactIcons />
