c7759ecee65761dae8d7c431df3405c08febf778
rana-m-h <rana29638@gmail.com>
2025-04-27 23:18:03 +0300

    feat/(orders): Applying enhancements

--- src/lib/apis/orders.api.ts
+++ src/lib/apis/orders.api.ts
@@ -1,24 +1,17 @@
 import { JSON_HEADER } from "@/lib/constants/api.constant";
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export async function fetchOrders() {
   // Get the token from cookies
   const token = cookies().get("next-auth.session-token")?.value;
 
-  // Get the secret key used to decode the JWT
-  const secret = process.env.NEXTAUTH_SECRET;
-
-  if (!secret) throw new Error("NEXTAUTH_SECRET is not defined");
-
-  // If there's no token, redirect to home
   if (!token) {
-    redirect("/");
+    throw new Error("Authentication token is missing. Please login to view your orders.");
   }
 
   // Decode the session token to extract user information
-  const userToken = await decode({ secret, token });
+  const userToken = await decode({ secret: process.env.NEXTAUTH_SECRET!, token });
 
   const response = await fetch(process.env.API + `/orders`, {
     headers: { ...JSON_HEADER, Authorization: `Bearer ${userToken?.token}` },
