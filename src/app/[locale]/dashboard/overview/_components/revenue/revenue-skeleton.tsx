04a20cffecdafd2b671b965ea4975fe5e40eefdf
rana-m-h <rana29638@gmail.com>
2025-05-10 23:55:24 +0300

    feat/(Implement-dashboard-overview-B): Applying enhancements, creat skeleton

--- src/app/[locale]/dashboard/overview/_components/revenue/revenue-skeleton.tsx
+++ src/app/[locale]/dashboard/overview/_components/revenue/revenue-skeleton.tsx
@@ -6,12 +6,14 @@ export default function RevenueSkeleton() {
       {/* Chart skeleton */}
       <div className="flex gap-6">
         <Skeleton className="w-[276px] h-[381px] rounded-xl" />
+
         <Skeleton className="flex-1 h-[381px] rounded-xl" />
       </div>
 
       {/* Table skeletons */}
       <div className="flex gap-6">
         <Skeleton className="w-[536px] h-[300px] rounded-xl" />
+
         <Skeleton className="w-[536px] h-[300px] rounded-xl" />
       </div>
     </div>
