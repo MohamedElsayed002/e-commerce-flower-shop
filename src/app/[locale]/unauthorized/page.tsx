6e82364d534b8f9a0750f67e0e1f98f000f64793
rana-m-h <rana29638@gmail.com>
2025-05-15 07:41:57 +0300

    feat/(occasion-middleware): create update add occasion and update middleware

--- src/app/[locale]/unauthorized/page.tsx
+++ src/app/[locale]/unauthorized/page.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { getTranslations } from "next-intl/server";
 
-export default function Unauthorized() {
+export default async function Unauthorized() {
+  // Translation
+  const t = await getTranslations();
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center px-4 text-center">
       {/* Image */}
@@ -16,17 +20,17 @@ export default function Unauthorized() {
 
       {/* Text */}
       <h1 className="text-2xl font-semibold text-gray-800 mb-2">
-        You are not authorized to access this page.
+        {t("you-are-not-authorized-to-access-this-page")}
       </h1>
 
       {/* Text */}
-      <p className="text-gray-500 ">If you think this is wrong, please contact support.</p>
+      <p className="text-gray-500 ">{t("if-you-think-this-is-wrong-please-contact-support")}</p>
 
       <div className="border-t border-gray-300 my-4 w-full max-w-sm mx-auto" />
       {/* Button */}
       <Link href="/">
         <Button className="bg-wh text-black rounded-lg shadow-sm border px-6 py-2 hover:bg-transparent">
-          Go to Homepage
+          {t("go-to-homepage")}
         </Button>
       </Link>
     </div>
